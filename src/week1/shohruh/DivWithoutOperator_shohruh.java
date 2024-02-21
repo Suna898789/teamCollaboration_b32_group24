0f843db887959e2be7d54a509bbed9ebd278009c
shohruh0312 <bshohruh92@gmail.com>
2024-02-21 12:13:43 -0500

    shohruh task solutions for week 1

---
+++ src/week1/shohruh/DivWithoutOperator_shohruh.java
@@ -0,0 +1,27 @@
+package shohruh;
+
+public class DivWithoutOperator_shohruh {
+
+    public static void main(String[] args) {
+        divWithoutOperator(45,22);
+    }
+        public static void divWithoutOperator(int num1, int num2){
+
+            if(num2 == 0){
+                System.err.println("CAN NOT DIVIDE BY ZERO");
+                return;
+            }
+
+            int count = 0;
+            String result = num1+"/"+num2+" = ";
+            String remainder = num1+"%"+num2+" = ";
+
+            while (num1 >= num2) {
+                num1 -= num2;
+                count++;
+            }
+            System.out.println(result +count + "\n"+remainder + num1);
+        }
+
+    }
+
