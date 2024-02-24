19914e816de8e9aac1a37f25a6106aa272424b32
maryemamstine <amstinemeryem77@gmail.com>
2024-02-24 16:44:35 -0500

    Meryem tasks solution Week2

---
+++ src/week2/meryem/SwapNumbers_meryem.java
@@ -0,0 +1,46 @@
+//** created by Maryam 
+
+
+package src.week2.meryem;
+
+public class SwapNumbers_meryem {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 15;
+        //Logic 1 - use + & - without using third variable;
+
+        a = a + b;//  5 + 15 = 20 update
+        b = a - b;//  20 - 15 =  5
+        a = a - b;//  20 - 5 = 15
+
+
+        System.out.println("a = " + a);//15
+        System.out.println("b = " + b);// 5
+
+
+
+
+        System.out.println("================================================");
+
+        //Logic 2 use * & / without using third variable;
+/*
+            a = a * b;//  5 * 15 = 75
+            b = a / b;//  75 / 15 = 5
+            a = a / b;//   75 / 5 = 15
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+
+ */
+
+    }
+
+}
+/*
+Swap two variable values without using a third variable
+ */
+
+
