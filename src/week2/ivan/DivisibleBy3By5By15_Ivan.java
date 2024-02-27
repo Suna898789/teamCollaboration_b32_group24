6e479ad22f96f2dc9f0a4719c12cd1bf82fa4337
Ivan <kinca30.03@gmail.com>
2024-02-27 11:24:16 -0500

    Ivan's Tasks Solutions for week2 and week3

---
+++ src/week2/ivan/DivisibleBy3By5By15_Ivan.java
@@ -0,0 +1,4 @@
+package week2.ivan;
+
+public class DivisibleBy3By5By15_Ivan {
+}
---
+++ src/week2/ivan/Swap_ivan.java
@@ -0,0 +1,22 @@
+package week2.ivan;
+
+public class Swap_ivan {
+
+    public static void main(String[] args) {
+        int n1=2;
+        int n2=5;
+
+        n1=n2+n1;
+
+        n2=n1-n2;
+
+        n1=n1-n2;
+
+        System.out.println("n1 = " + n1);
+
+        System.out.println("n2 = " + n2);
+
+
+
+    }
+}
