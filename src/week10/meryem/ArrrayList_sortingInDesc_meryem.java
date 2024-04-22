89e0b15066a43844468ebb4d576298e19fe91ecf
maryemamstine <amstinemeryem77@gmail.com>
2024-04-22 01:52:35 -0400

    Meryem tasks solution Week10

---
+++ src/week10/meryem/ArrrayList_sortingInDesc_meryem.java
@@ -0,0 +1,20 @@
+//** created by Maryam 
+
+
+package src.week10.meryem;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrrayList_sortingInDesc_meryem {
+
+    /*
+    Write a method that can sort the ArrayList in Descending order without using the sort method.
+     */
+    public static void main(String[] args) {
+
+      //  Write a method that can sort the ArrayList in Descending order without using the sort method.
+
+
+    }
+}
